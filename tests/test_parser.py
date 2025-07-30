9afc049c215f3dd5a788aad49e75507666d88e6c
Eilert-peilert <eilert-t@hotmail.com>
2025-07-30 17:13:13 +0200

    Added more unit tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,14 +1,34 @@
 import pytest
+import json
 from helper_functions import parse_json_data
 
 
 def test_parse_country_names_valid_json():
-    json_str = """
-    [
+    country_list = [
         {"name": "Brazil"},
         {"name": "Canada"},
         {"name": "Australia"}
     ]
-    """
+    json_str = json.dumps(country_list)
     expected = ["Australia", "Brazil", "Canada"]
     assert parse_json_data(json_str) == expected
+
+
+def test_parse_country_names_missing_field():
+    country_list = [
+        {"wrong": "value"},
+        {"name": "Norway"}
+    ]
+    json_str = json.dumps(country_list)
+    assert parse_json_data(json_str) == ["Norway"]
+
+
+def test_parse_country_names_empty_list():
+    country_list = []
+    json_str = json.dumps(country_list)
+    assert parse_json_data(json_str) == []
+
+
+def test_parse_country_names_invalid_json():
+    with pytest.raises(Exception):
+        parse_json_data("invalid-json")
